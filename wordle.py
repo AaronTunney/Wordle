c398231aefa99ef65e2593ed24b8f191cf17efd2
Aaron Tunney <aaron.tunney@comparethemarket.com>
2022-06-08 16:19:05 +0100

    Implement solution

--- wordle.py
+++ wordle.py
@@ -5,11 +5,23 @@ class Wordle:
 
     def __init__(self, target):
         """ Standard initialiser. """
-        self.target = target
+        self.target = list(target)
 
     def guess(self, guess):
         """ Guess at the target word. """
-        return "00000"
+
+        result = ["0", "0", "0", "0", "0"]
+
+        for index, character in enumerate(list(guess)):
+            guess_list = list(guess)
+            character_count = guess_list[:index].count(character)
+
+            if character == self.target[index]:
+                result[index] = "2"
+            elif self.target.count(character) > character_count:
+                result[index] = "1"
+
+        return ''.join(result)
 
 if __name__ == '__main__':
     wordle = Wordle('hello')
